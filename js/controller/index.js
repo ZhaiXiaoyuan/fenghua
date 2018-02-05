40c3863d82c41109eb4ba5f4c06811a177f26fc4
huangchunrong <Huangchunrong@leyaoyao.com>
2018-02-05 17:36:27 +0800

    20180205

--- js/controller/index.js
+++ js/controller/index.js
@@ -2,5 +2,30 @@
  * Created by Administrator on 2018/2/4 0004.
  */
 $(function () {
+   /* var params={
+        timestamp:utils.genTimestamp(),
+        number:'13751839133',
+        password:'123456'
+    }
+    api.login(params,function (resp) {
 
+    })*/
+
+    /**
+     * 查询任务列表
+     */
+    var taskData=null;
+    function getTaskList() {
+        var params={
+            timestamp:utils.genTimestamp(),
+            number:'13751839133',
+            status:'Queue,Waiting,Working,Pause,Complete'
+        }
+        api.getTaskList(params,function (resp) {
+            console.log('resp:',resp);
+            if(resp.status=='success'){
+            }
+        })
+    }
+    getTaskList();
 })
---
+++ js/controller/login.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Designer on 2018/2/5.
+ */
